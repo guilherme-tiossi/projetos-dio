d85f388d26423ec543b3d3c5baf09e6dd91ba511
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-03-14 08:27:18 -0300

    Refatoração no estudo de Nós

--- ProjetosDio/src/EstudoNos/Main.java
+++ ProjetosDio/src/EstudoNos/Main.java
@@ -1,12 +1,12 @@
 package EstudoNos;
 public class Main {
     public static void main(String[] args){
-        No<Integer> no1 = new No("Conteúdo no1");
-        No<String> no2 = new No("Conteúdo no2");
+        No<String> no1 = new No<String>("Conteúdo no2");
+        No<String> no2 = new No<String>("Conteúdo no2");
         no1.setProximoNo(no2);
-        No<String> no3 = new No("Conteúdo no3");
+        No<String> no3 = new No<String>("Conteúdo no3");
         no2.setProximoNo(no3);
-        No<String> no4 = new No("Conteúdo no4");
+        No<String> no4 = new No<String>("Conteúdo no4");
         no3.setProximoNo(no4);
         System.out.println(no3);
     }
